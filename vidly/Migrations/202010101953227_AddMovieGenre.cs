d582845b395508c6f0496482eb66ca9593b9a26f
Warren Keil <wdkeil@gmail.com>
2020-10-10 14:55:42 -0500

    messed up

---
+++ vidly/Migrations/202010101953227_AddMovieGenre.cs
@@ -0,0 +1,16 @@
+namespace vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMovieGenre : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
