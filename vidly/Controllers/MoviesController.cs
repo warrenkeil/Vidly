c96a49402b6f6a9ec002f7d11e92654ea286a713
Warren Keil <wdkeil@gmail.com>
2020-10-09 22:10:24 -0500

    exer2 finished

--- vidly/Controllers/MoviesController.cs
+++ vidly/Controllers/MoviesController.cs
@@ -6,58 +6,46 @@ using System.Web.Mvc;
 using vidly.Models;
 using vidly.ViewModels;
 
+
+
+
 namespace vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer { Name = "Customer 1",},
-                new Customer { Name = "Customer 2",}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
-
 
             var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
             };
-                    
+
             return View(viewModel);
-
-            
-        }
-
-
-
-
-
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
         }
     }
 }
